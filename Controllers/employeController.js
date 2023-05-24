24a8e00a75316cdc02f0646738242576b3c93a89
abd-ellatif <ka_bendjeddou@esi.dz>
2023-05-24 09:44:34 +0100

    hh

--- Controllers/employeController.js
+++ Controllers/employeController.js
@@ -3,19 +3,21 @@ const employeService = require("../Services/employeService.js");
 
 const employeController = express.Router();
 
-employeController.get("/AfficherDemandes", async (req, res) => {
+employeController.get("/AfficherDemandes", async (req, res, next) => {
   try {
     const result = await employeService.AfficherDemandes();
     res.status(200).send(result);
+    next();
   } catch (e) {
     res.status(500).send(e.message);
   }
 });
 
-employeController.post("/AccepterDemande/:idPI", async (req, res) => {
+employeController.post("/AccepterDemande/:idPI", async (req, res, next) => {
   try {
     await employeService.AccepterDemande(req.params.idPI);
     res.status(200).send("Demande acceptée avec succés");
+    next();
   } catch (e) {
     console.log(e);
     res.status(500).send(e.message);
--- Controllers/responsableController.js
+++ Controllers/responsableController.js
@@ -4,28 +4,33 @@ const responsableService = require("../Services/responsableService");
 const responsableController = express.Router();
 
 
-responsableController.post("/ajouterLieu/:idResponsable", async (req, res) => {
-  try {
-    await responsableService.AjouterLieu(
-      req.params.idResponsable,
-      req.body.lieu,
-      req.body.photos,
-      req.body.themes,
-      req.body.categories,
-      req.body.horaires,
-      req.body.arretsTransport
-    );
-  } catch (error) {
-    console.error(error);
+responsableController.post(
+  "/ajouterLieu/:idResponsable",
+  async (req, res, next) => {
+    try {
+      await responsableService.AjouterLieu(
+        req.params.idResponsable,
+        req.body.lieu,
+        req.body.photos,
+        req.body.themes,
+        req.body.categories,
+        req.body.horaires,
+        req.body.arretsTransport
+      );
+    } catch (error) {
+      console.error(error);
+    }
+    res.status(200).send("Lieu ajouté avec succés");
+    next();
   }
-  res.status(200).send("Lieu ajouté avec succés");
-});
+);
 
 // Pas implementé
-responsableController.post("/ModifierLieu/:idPI", async (req, res) => {
+responsableController.post("/ModifierLieu/:idPI", async (req, res, next) => {
   try {
     await responsableService.ModifierLieu(req.params.idPI);
     res.status(200).send("Lieu modifié avec succés");
+    next();
   } catch (e) {
     res.status(500).send(e.message);
   }
@@ -33,10 +38,11 @@ responsableController.post("/ModifierLieu/:idPI", async (req, res) => {
 
 responsableController.delete(
   "/SupprimerCommentaire/:idCommentaire",
-  async (req, res) => {
+  async (req, res, next) => {
     try {
       await responsableService.SupprimerCommentaire(req.params.idCommentaire);
       res.status(200).send("Commentaire supprimé avec succés");
+      next();
     } catch (e) {
       console.error(e);
       res.status(500).send(e.message);
@@ -44,30 +50,36 @@ responsableController.delete(
   }
 );
 
-responsableController.post("/AjouterQuiz/:idPI", async (req, res) => {
+responsableController.post("/AjouterQuiz/:idPI", async (req, res, next) => {
   try {
     await responsableService.AjouterQuiz(req.params.idPI, req.body);
     res.status(200).send("Quiz ajouté avec succés");
+    next();
   } catch (e) {
     console.error(e);
     res.status(500).send(e.message);
   }
 });
 
-responsableController.post("/AjouterEvenement/:idPI", async (req, res) => {
-  try {
-    await responsableService.AjouterEvenement(req.params.idPI, req.body);
-    res.status(200).send("Evenement ajouté avec succés");
-  } catch (e) {
-    console.error(e);
-    res.status(500).send(e.message);
+responsableController.post(
+  "/AjouterEvenement/:idPI",
+  async (req, res, next) => {
+    try {
+      await responsableService.AjouterEvenement(req.params.idPI, req.body);
+      res.status(200).send("Evenement ajouté avec succés");
+      next();
+    } catch (e) {
+      console.error(e);
+      res.status(500).send(e.message);
+    }
   }
-});
+);
 
-responsableController.post("/AjouterOffre/:idPI", async (req, res) => {
+responsableController.post("/AjouterOffre/:idPI", async (req, res, next) => {
   try {
     await responsableService.AjouterOffre(req.params.idPI, req.body);
     res.status(200).send("Offre ajoutée avec succés");
+    next();
   } catch (e) {
     console.error(e);
     res.status(500).send(e.message);
